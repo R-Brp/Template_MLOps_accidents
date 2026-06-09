c824f2364a85c90df59dcdf3307afebf59f59fdb
R-Brp <beaurepere.romane@gmail.com>
2026-06-09 16:37:23 +0200

    Final version with all data preprocessing transformations

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -57,7 +57,7 @@ def process_data(input_filepath_users, input_filepath_caract, input_filepath_pla
     
     df = add_new_columns(df, nb_victim, nb_vehicules)
 
-    # Modify target variable
+  
     df = modif_target_variable(df)
 
     # Replace values -1 and 0
@@ -66,7 +66,7 @@ def process_data(input_filepath_users, input_filepath_caract, input_filepath_pla
     # Drop columns
     df = drop_columns(df)
 
-    # Drop lines with NaN values
+  
     df = drop_lines_with_nan_values(df)
 
     # Split data into training and testing sets
